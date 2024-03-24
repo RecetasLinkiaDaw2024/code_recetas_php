40d03ced052d86cacf6ee11b641deb422097687b
MariaLosRosales <mgsgonzalez@gmail.com>
2024-03-24 12:47:49 +0100

    Correcciones para que todo vuelva a funcionar. Se añaden estilos que no se han subido para el dropdown. se le quita contenido a BotonMenu, todo se puede hacer desde header.php

--- public/header.php
+++ public/header.php
@@ -1,8 +1,46 @@
 <?php
 require_once(__DIR__."/../config/settings.php");
-require_once(__DIR__."/../images/botonMenu.php");
 ?>
+
+<!-- dos opciones, con usuario logado y sin el.
+    una tiene el menu y la otra los botones de login-->
+
+<?php if (isset($_SESSION[SESSION_USER])) { ?> 
+    <!-- OPCION con USUARIO-->
 <div class="back">
+    <div class="menu container">
+        <div class="logo">
+            <img src="<?= DEPLOY_PATH?>/public/images/logofinal.jpg" alt="logo">
+        </div>
+        <input type="checkbox" id="menu" />
+        <label for="menu">
+            &#9776
+        </label>
+        <nav class="navbar">
+            <ul>
+                <li class="dropdown">
+                    <a href="#" class="dropbtn">Menú de la aplicación</a>
+                    <div class="dropdown-content">
+                        <a href="misDatos.php">Mis datos</a>
+                        <a href="detalle_receta.php">Mis recetas</a>
+                        <a href="#">Buscador de recetas</a>
+                        <?php
+                            // Verificar si el usuario es administrador
+                            $isAdmin = false; // Supongamos que esta es la lógica para determinar si el usuario es administrador o no
+                            if ($isAdmin) {
+                                echo '<a href="#" id="adminOption">Administración de usuarios</a>';
+                            }
+                        ?>
+                        <a href="#">Cerrar sesión</a>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</div>
+<?php } else { ?> 
+<!-- OPCION SIN USUARIO-->
+    <div class="back">
             <div class="menu container">
                 <div class="logo">
                     <img src="<?= DEPLOY_PATH?>/public/images/logofinal.jpg" alt="logo">
@@ -19,3 +57,4 @@ require_once(__DIR__."/../images/botonMenu.php");
                 </nav>
             </div>
         </div>
+        <?php } ?>         
